4794399d8d59210a0a2a65281a0c0ae625e7eec6
dornascarol <carolinafdornas@gmail.com>
2024-03-30 23:36:30 -0300

    instala @fastify/websocket

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,5 +1,7 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
+import websocket from '@fastify/websocket'
+
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
 import { voteOnPoll } from './routes/vote-on-poll'
@@ -10,6 +12,8 @@ app.register(cookie, {
     secret: "polls-app-nlw",
     hook: 'onRequest',
 })
+
+app.register(websocket)
 
 app.register(createPoll)
 app.register(getPoll)
