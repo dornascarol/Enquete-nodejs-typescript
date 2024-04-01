dfd3c81de70b6798b4e51214a9f758981c816c52
dornascarol <carolinafdornas@gmail.com>
2024-03-31 23:24:22 -0300

    adiciona rota get poll-results

--- src/http/ws/poll-results.ts
+++ src/http/ws/poll-results.ts
@@ -1,5 +1,9 @@
 import { FastifyInstance } from "fastify";
 
 export async function pollResults(app: FastifyInstance) {
-    app.get('/polls/:pollId/results')
+    app.get('/polls/:pollId/results', { websocket: true }, (connection, request) => {
+        connection.socket.on('message', (message: string) => {
+            connection.socket.send('Você enviou: ' + message )
+        })
+    })
 }
